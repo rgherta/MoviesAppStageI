b7b6b0b4f4a75b91e3c481e3708af3279a3af2fd
Roman Gherta <rgherta@yahoo.com>
2017-02-19 12:57:51 +0200

    Initial commit

---
+++ app/src/main/java/com/github/moviesappstagei/moviesappstagei/MovieGallery.java
@@ -0,0 +1,13 @@
+package com.github.moviesappstagei.moviesappstagei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_gallery);
+    }
+}
